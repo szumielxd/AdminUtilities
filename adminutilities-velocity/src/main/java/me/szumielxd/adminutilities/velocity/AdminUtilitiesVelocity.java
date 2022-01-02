94cb8792f8b074e228d3d8b83645d544a251f896
szumielxd <43210079+szumielxd@users.noreply.github.com>
2022-01-02 22:06:53 +0100

    Messed up with dependencies

--- adminutilities-velocity/src/main/java/me/szumielxd/adminutilities/velocity/AdminUtilitiesVelocity.java
+++ adminutilities-velocity/src/main/java/me/szumielxd/adminutilities/velocity/AdminUtilitiesVelocity.java
@@ -13,6 +13,7 @@ import com.velocitypowered.api.command.CommandManager;
 import com.velocitypowered.api.command.CommandMeta;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.plugin.Dependency;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.ProxyServer;
@@ -40,12 +41,16 @@ import me.szumielxd.adminutilities.velocity.listeners.ChatListener;
 import me.szumielxd.adminutilities.velocity.objects.VelocityProxy;
 
 @Plugin(
-		id = "${parent.artifactId}",
-		name = "${pluginName}",
-		version = "${version}",
-		authors = { "${author}" },
-		description = "${description}",
-		url = "${parent.url}"
+		id = "id----",
+		name = "@pluginName@",
+		version = "@version@",
+		authors = { "@author@" },
+		description = "@description@",
+		url = "https://github.com/szumielxd/AdminUtilities/",
+		dependencies = {
+				@Dependency(id = "luckperms", optional = true),
+				@Dependency(id = "szlobbysystem", optional = true)
+		}
 )
 public class AdminUtilitiesVelocity implements AdminUtilities {
 	
