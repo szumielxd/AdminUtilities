df2347a0021f2d04ce358efd95b197a90daa2e84
szumielxd <43210079+szumielxd@users.noreply.github.com>
2024-05-14 23:17:06 +0200

    Fixed chat saving for reports

--- adminutilities-velocity/src/main/java/me/szumielxd/adminutilities/velocity/listeners/ChatListener.java
+++ adminutilities-velocity/src/main/java/me/szumielxd/adminutilities/velocity/listeners/ChatListener.java
@@ -1,15 +1,17 @@
 package me.szumielxd.adminutilities.velocity.listeners;
 
+import java.util.Optional;
+
 import com.velocitypowered.api.event.PostOrder;
 import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.command.CommandExecuteEvent;
 import com.velocitypowered.api.event.player.PlayerChatEvent;
 import com.velocitypowered.api.event.player.PlayerChatEvent.ChatResult;
+import com.velocitypowered.api.proxy.Player;
 
 import me.szumielxd.adminutilities.common.AdminUtilities;
 import me.szumielxd.adminutilities.common.configuration.Config;
 import me.szumielxd.adminutilities.common.data.ChatPlayer;
-import me.szumielxd.adminutilities.common.managers.AdminChatManager.ChannelType;
-import me.szumielxd.adminutilities.common.objects.CommonPlayer;
 
 public class ChatListener {
 	
@@ -21,40 +23,43 @@ private final AdminUtilities plugin;
 		this.plugin = plugin;
 	}
 	
-	
 	// SAVE CHAT TO CACHE
-	@Subscribe(order = PostOrder.NORMAL)
-	public void chatSaver(PlayerChatEvent event) {
-		if (!event.getResult().isAllowed()) return;
-		if (event.getMessage() == null) return;
-		
-		ChatPlayer cp = ChatPlayer.getChatPlayer(event.getPlayer().getUniqueId());
-		if (cp == null) return;
-		if (!event.getMessage().startsWith("/")) {
-			String[] args = event.getMessage().substring(1).split(" ", 2);
-			if (args.length == 0) return;
-			if (args[0].isEmpty()) return;
-			if (Config.IGNORE_NAMESPACES.getBoolean() && args[0].indexOf(':') >= 0) args[0] = args[0].substring(args[0].indexOf(':') + 1);
-			if (!Config.LISTENED_COMMANDS.getStringList().contains(args[0].toLowerCase())) return;
+	@Subscribe(order = PostOrder.LAST)
+	public void commandSaver(PlayerChatEvent event) {
+		if (event.getResult().isAllowed()) {
+			Optional.ofNullable(ChatPlayer.getChatPlayer(event.getPlayer().getUniqueId())).ifPresent(cp -> {
+				cp.append(System.currentTimeMillis(), event.getMessage());
+			});
 		}
-		cp.append(System.currentTimeMillis(), event.getMessage());
+	}
+	
+	// SAVE COMMANDS TO CACHE
+	@Subscribe(order = PostOrder.LAST)
+	public void commandSaver(CommandExecuteEvent event) {
+		if (event.getResult().isAllowed() && event.getCommandSource() instanceof Player player) {
+			Optional.ofNullable(ChatPlayer.getChatPlayer(player.getUniqueId())).ifPresent(cp -> {
+				String[] args = event.getCommand().split(" ", 2);
+				if (Config.IGNORE_NAMESPACES.getBoolean() && args[0].indexOf(':') >= 0) {
+					args[0] = args[0].substring(args[0].indexOf(':') + 1);
+				}
+				if (Config.LISTENED_COMMANDS.getStringList().contains(args[0].toLowerCase())) {
+					cp.append(System.currentTimeMillis(), '/' + event.getCommand());
+				}
+			});
+		}
 	}
 	
 	
 	// Listen AdminChannel
 	@Subscribe(order = PostOrder.LATE)
 	public void adminChatMonitor(PlayerChatEvent event) {
-		
-		if (event.getMessage() == null || event.getMessage().isEmpty()) return;
-		if (!event.getResult().isAllowed()) return;
-		if (event.getMessage().startsWith("/")) return;
-		
-		CommonPlayer player = this.plugin.getProxyServer().getPlayer(event.getPlayer().getUniqueId());
-		ChannelType channel = this.plugin.getAdminChatManager().getChannelByMessage(player, event.getMessage());
-		if (channel == null) return;
-		if (!this.plugin.hasPermAndNotLobby(channel.getPermission()).test(player)) return;
-		this.plugin.getAdminChatManager().sendMessage(channel, player, event.getMessage().substring(channel.getPrefix().length()));
-		event.setResult(ChatResult.denied());
+		if (event.getResult().isAllowed()) {
+			Optional.ofNullable(this.plugin.getProxyServer().getPlayer(event.getPlayer().getUniqueId())).ifPresent(player -> 
+					this.plugin.getAdminChatManager().getChannelByMessage(player, event.getMessage()).ifPresent(channel -> {
+							this.plugin.getAdminChatManager().sendMessage(channel, player, event.getMessage().substring(channel.getPrefix().length()));
+							event.setResult(ChatResult.denied());
+					}));
+		}
 	}
 	
 
